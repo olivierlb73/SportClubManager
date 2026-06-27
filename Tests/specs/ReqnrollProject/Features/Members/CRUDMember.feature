708ddf02766bcc01585fb8b0c6feb93c67dc5efa
Olivier Le Borgne <olivier.leborgne7@gmail.com>
2026-06-27 15:04:37 +0000

    Complete Reqnroll CRUD test on members API

--- Tests/specs/ReqnrollProject/Features/Members/CRUDMember.feature
+++ Tests/specs/ReqnrollProject/Features/Members/CRUDMember.feature
@@ -1,7 +1,14 @@
 Feature: Create, Retrieve, Update and Delete a new member
 
-  Scenario: Create and retrieve a new member
+  Scenario: Create, Retrieve, Update and Delete a new member
     When I create a new member with
       | firstName | lastName | email              |
       | John      | Doe      | john.doe@gmail.com |
     Then I can retrieve the new member
+    And I can retrieve the list of members
+    When I update the new member with
+      | firstName | lastName | email                   |
+      | Johnny    | Begood   | johnny.begood@gmail.com |
+    Then I can retrieve the new member
+    When I delete the new member
+    Then I can not retrieve the new member
--- Tests/specs/ReqnrollProject/StepDefinitions/Members/CRUDMemberStepDefinitions.cs
+++ Tests/specs/ReqnrollProject/StepDefinitions/Members/CRUDMemberStepDefinitions.cs
@@ -7,6 +7,8 @@ using NuGet.Protocol;
 using SportClubManager.Core.Application.Members.Queries.GetMemberById;
 using SportClubManager.Core.Application.Members.Queries;
 using Microsoft.AspNetCore.Mvc.Testing;
+using SportClubManager.Core.Application.Members.Commands.UpdateMember;
+using System.Net;
 
 namespace ReqnrollProject.StepDefinitions.Members;
 
@@ -26,9 +28,9 @@ public class CRUDMemberStepDefinitions
     public async Task ICreateANewMemberWith(Table table)
     {
         var row = table.Rows[0];
-        CreateMemberCommand createMemberCommand = new (row["firstName"], row["lastName"], row["email"]);
+        CreateMemberRequest createMemberRequest = new (row["firstName"], row["lastName"], row["email"]);
             
-        var result = await _httpClient.PostAsJsonAsync("/api/members", createMemberCommand);
+        var result = await _httpClient.PostAsJsonAsync("/api/members", createMemberRequest);
 
         result.EnsureSuccessStatusCode();
         _memberId = await result.Content.ReadFromJsonAsync<long>();
@@ -39,6 +41,7 @@ public class CRUDMemberStepDefinitions
     public async Task ICanRetrieveTheNewMember()
     {
       var memberResponse = await _httpClient.GetAsync($"/api/members/{_memberId}");
+
       memberResponse.EnsureSuccessStatusCode();
 
       var member = await memberResponse.Content.ReadFromJsonAsync<MemberResponse>();
@@ -47,6 +50,47 @@ public class CRUDMemberStepDefinitions
       Assert.That(member!.Id, Is.EqualTo(_expectedMemberResponse!.Id));
       Assert.That(member.FirstName, Is.EqualTo(_expectedMemberResponse.FirstName));
       Assert.That(member.LastName, Is.EqualTo(_expectedMemberResponse.LastName));
+    }
+
+    [Then("I can retrieve the list of members")]
+    public async Task ICanRetrieveTheListOfMembers()
+    {
+      var membersResponse = await _httpClient.GetAsync($"/api/members");
+
+      membersResponse.EnsureSuccessStatusCode();
+
+      var members = await membersResponse.Content.ReadFromJsonAsync<IEnumerable<MemberResponse>>();
+
+      Assert.That(members, Has.Count.GreaterThan(0));
+    }
+    
+    [When("I update the new member with")]
+    public async Task IUpdateTheNewMemberWith(Table table)
+    {
+        var row = table.Rows[0];
+        UpdateMemberRequest updateMemberRequest = new (row["firstName"], row["lastName"], row["email"]);
+            
+        var result = await _httpClient.PutAsJsonAsync($"/api/members/{_memberId}", updateMemberRequest);
+
+        result.EnsureSuccessStatusCode();
+        
+        _expectedMemberResponse = new MemberResponse(_memberId, row["firstName"], row["lastName"]);
+    }
+
+    [When("I delete the new member")]
+    public async Task IDeleteTheNewMember()
+    {
+        var memberResponse = await _httpClient.DeleteAsync($"/api/members/{_memberId}");
+
+        memberResponse.EnsureSuccessStatusCode();
+    }
+
+    [Then("I can not retrieve the new member")]
+    public async Task ICanNotRetrieveTheNewMember()
+    {
+      var memberResponse = await _httpClient.GetAsync($"/api/members/{_memberId}");
+
+      Assert.That(memberResponse.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
     }
 
 }
